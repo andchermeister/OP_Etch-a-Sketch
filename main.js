923968df0b2359266b571cacba938d9a8ac4c105
Andrii Chernikov <nbk.chernikov@gmail.com>
2024-02-08 15:00:31 +0000

    Added coloring with 'mouseover', added buttons functional, random color and 'Coloring mode'

--- main.js
+++ main.js
@@ -1,19 +1,64 @@
-function setBoard(size) {
-    let board = document.querySelector(".board");
+let board = document.querySelector(".board");
+let color = "black"
+let click = true
 
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+function setBoard(size) {
+
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
 
     board_size = size * size
 
     for (let i = 0; i < board_size; i++) {
         let board_element = document.createElement('div')
-        board_element.style.backgroundColor = "blue"
+        board_element.style.backgroundColor = "white"
+        board_element.addEventListener("mouseover", colorDiv)
         board.insertAdjacentElement("beforeend", board_element)
     }
+
+
 }
 
 setBoard(16)
+
+function setBoardSize(input) {
+    if (input >= 4 && input <= 100) {
+        setBoard(input)
+    } else {
+        alert("Size of board is inappropriate! Try again.")
+    }
+}
+
+function colorDiv() {
+    if (click) {
+        if (color === 'random') {
+            this.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
+        } else {
+            this.style.backgroundColor = color
+        }
+    }
+}
+
+function changeColor(choice) {
+    color = choice
+}
+
+function reset() {
+    let board_elements = document.querySelectorAll('div')
+    board_elements.forEach((div) => div.style.backgroundColor = "white")
+}
+
+document.querySelector('body').addEventListener('click', () => {
+    click = !click
+    if (click) {
+        document.querySelector('.mode').textContent = "Mode: Coloring"
+    } else {
+        document.querySelector('.mode').textContent = "Mode: Not coloring"
+    }
+    
+})
+
+
 
 
 
